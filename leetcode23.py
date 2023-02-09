d402ce396340400fedb74c50bd9282540cb88e3a
abhijeeth-babu <121789736+abhijeeth-babu@users.noreply.github.com>
2023-02-09 18:29:04 +0530

    Create leetcode23.py

---
+++ leetcode23.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.merge_two(l1, l2))
+            lists = mergedLists
+        return lists[0]
+
+    def merge_two(self, list1, list2):
+        new_head = ListNode()
+        temp = new_head
+        while list1 and list2:
+            if list1.val < list2.val:
+                temp.next = list1
+                list1 = list1.next
+            else:
+                temp.next = list2
+                list2 = list2.next
+            temp = temp.next
+        temp.next = list1 if list1 else list2
+        return new_head.next
